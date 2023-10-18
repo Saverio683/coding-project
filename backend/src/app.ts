e3749f7599483e21e0eaf5c5f44cd367380b4f1e
Saverio683 <saveriorandazzo50@gmail.com>
2023-10-18 20:52:24 +0200

    ver

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express'
 import cors from 'cors'
+import http from 'http'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,6 +8,7 @@ dotenv.config()
 import countryRouter from './routes/countries/countries.route' 
 
 const app: Express = express()
+const server = http.createServer(app)
 
 const corsOptions: cors.CorsOptions = {
     origin: "*"
@@ -18,6 +20,15 @@ app.use(express.json({limit: '500mb'}))
 const PORT: number = parseInt(process.env.PORT || '4000', 10)
 
 app.use('/countries', countryRouter)
-app.listen()
 
+const startServer = async (): Promise<void> => {
+    try {
+        server.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}...`)
+        })
+    } catch (err) {
+        console.error(err)
+    }   
+}
+startServer()
 export default app
